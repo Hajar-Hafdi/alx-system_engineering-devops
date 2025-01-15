17c323c7e46d63d93d8b677bc31e77371a13e554
Hajar Hafdi <hajarlakbir@outlook.com>
2025-01-15 15:33:58 -0800

    task 0 & 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,13 @@
+# install flask web framework
+package {'install_flask':
+ensure   => '2.1.0',
+name     => 'flask'    ,
+provider => pip3,
+}
+
+
+package {'install_werkzeug':
+ensure   => '2.1.1',
+name     => 'Werkzeug'    ,
+provider => pip3,
+}
